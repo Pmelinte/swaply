95a53ccaf86869a2f7f04f7914a97e108c5d9e6b
Pmelinte <pmelinte@gmail.com>
2025-10-01 23:23:52 +0300

    Update next.config.ts with new configuration

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,14 @@
-.
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  async redirects() {
+    return [{ source: "/loghin", destination: "/login", permanent: false }];
+  },
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
+  },
+};
+
+export default nextConfig;
